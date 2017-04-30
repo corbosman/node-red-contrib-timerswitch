36c693b049da2f097bbe25bc504f7cebbe315304
Cor Bosman <cor@xs4all.net>
2017-04-30 21:44:57 +0200

    two

--- timeswitch.js
+++ timeswitch.js
@@ -11,137 +11,152 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
 
-        /**
-         * running events
-         */
-        var events = { start: false, end: false};
-        var next = { on: false, off: false};
+        setTimeout(function(){
+            node.send({payload: 1});
+        },500);
+        node.send({payload: 2});
 
         /**
-         * initialise scheduler, get current schedule if any
+         * create scheduler
          */
-        var init = scheduler.init(config.schedules);
-        console.log('init');
-        console.log(init);
+        var schedules = scheduler.create(config.schedules);
 
-        this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+        /** start up incoming msg handler */
+        handleNodeEvents();
+
+        /** we have paused the schedule, dont fire ON/OFF event handlers */
+        if (config.paused) {
+            node.status({fill: 'red', shape: "dot", text: 'Paused'});
+            return;
+        }
+
+        if (scheduler.count() < 1) {
+            node.status({fill: 'red', shape: "dot", text: 'No schedules to run'});
+            return;
+        }
+
+        /* start off all the on events
+         * if the initial state is on, we fire an event in the past which starts immediately
+         */
+        var state = 'off';
+        var until = false;
+        schedules.forEach(function(s, i) {
+            if (s.alreadyRunning) {
+                state = 'on';
+                s.events.start = setTimeout(instantOn, scheduler.until(s.on), s, i);
+                until = (!until || s.off.getTime() < until.getTime() ? s.off : until);
+            } else {
+                s.events.start = setTimeout(turnOn, scheduler.until(s.on), s, i);
+                until = (!until || s.on.getTime() < until.getTime() ? s.on : until);
+            }
         });
 
+        /* set the initial status msg */
+        setStatus(state, until, false);
+
+        /* send initial msg */
+        sendMessage(state);
+
+        /* done */
+
+
+
 
         /**
-         * nothing to see here, move along
-         */
-        if (scheduler.count() < 1) return;
-
-        /**
-         * schedule the next ON event
-         */
-        scheduleOn();
-
-        /**
-         * send the current state
-         */
-        if (init !== false) {
-            /** fire an off event for the currently running schedule */
-            scheduleOff(init)
-            sendOn();
-        } else {
-            sendOff();
-        }
-
-        /**
-         * schedule the next event
-         */
-        function scheduleOn() {
-            console.log('scheduling on');
-            /**
-             * get the next start time
-             */
-            var s = scheduler.next();
-
-            /**
-             * clear timeout
-             */
-            if (events.start) {
-                clearTimeout(events.start);
-            }
-
-            /**
-             * new start event
-             */
-            events.start =  setTimeout(function(s) {
-                console.log('on timeout');
-                /** schedule current off */
-                scheduleOff(s);
-
-                /* send on msg */
-                sendOn();
-
-                /** schedule next on */
-                scheduleOn();
-
-            }, scheduler.diff(s.on), s);
-            console.log(events)
-            next.on = s.on;
-
-        }
-
-        /**
-         * schedule off event
+         * finish an already started ON, and fire a new event for the next ON
+         *
          * @param s
+         * @param i
          */
-        function scheduleOff(s) {
-            console.log('scheduling off');
-            console.log(s);
+        function instantOn(s,i) {
+            setStatus('on', s.off, false);
+            s.events.end   = setTimeout(turnOff, scheduler.until(s.off), s, i);
+            s.events.start = setTimeout(turnOn, scheduler.addDay(i), s, i);
+        }
 
-            if (events.end) {
-                clearTimeout(events.end);
+        /**
+         * turning ON, fire OFF event and new ON event, set status
+         * @param s
+         * @param i
+         */
+        function turnOn(s, i) {
+            setStatus('on', s.off, false);
+            sendMessage('on');
+            s.events.end   = setTimeout(turnOff, scheduler.duration(s.on, s.off), s, i);
+            s.events.start = setTimeout(turnOn, scheduler.addDay(i), s, i);
+        }
+
+        /**
+         * turning device OFF, setting status
+         * @param s
+         * @param i
+         */
+        function turnOff(s, i) {
+            sendMessage('off');
+            setStatus('off', scheduler.next().on, false);
+        }
+
+        /**
+         * set the node status
+         *
+         * @param state
+         * @param color
+         * @param d
+         */
+        function setStatus(state, until, manual) {
+            var color = state == 'on' ? 'green' : 'red';
+            var text = state + ' ' + (until ? ' until ' + statusTime(until) : (manual ? 'manual' : ''));
+
+            node.status({
+                fill: color,
+                shape: "dot",
+                text: text
+            });
+        }
+
+        /**
+         * send a msg to the next node
+         *
+         * @param state
+         */
+        function sendMessage(state) {
+            var msg;
+
+            if (state == 'on') {
+                msg = {
+                    payload: config.onpayload ? config.onpayload : state,
+                    topic: config.ontopic ? config.ontopic : ''
+                };
+            } else {
+                msg = {
+                    payload: config.offpayload ? config.offpayload : state,
+                    topic: config.offtopic ? config.offtopic : ''
+                };
             }
-
-            /** get the current schedule's duration */
-            var duration = scheduler.duration(s);
-
-            events.end = setTimeout(function() {
-                sendOff();
-            }, duration);
-            next.off = s.off;
-
+            console.log('sending msg');
+            console.log(msg);
+            node.send(msg);
         }
 
         /**
-         * send a new on msg
+         * node events
          */
-        function sendOn() {
-            console.log('on');
-            now();
-            console.log(next);
-            node.status({
-                fill: "green",
-                shape: "dot",
-                text: "On until " + statusTime(next.off)
+        function handleNodeEvents() {
+
+            /* handle incoming msgs */
+            node.on('input', function(msg) {
+                var command = msg.payload;
+
+                node.send(msg);
             });
-        }
 
-        /**
-         * send a new off msg
-         */
-        function sendOff() {
-            console.log('off');
-            now();
-            console.log(next);
-            node.status({
-                fill: "red",
-                shape: "dot",
-                text: "Off until " + statusTime(next.on)
+            /* clean up after close */
+            node.on('close', function() {
+                schedules.forEach(function(s) {
+                   if (s.events && s.events.start) clearTimeout(s.events.start);
+                   if (s.events && s.events.end) clearTimeout(s.events.end);
+               });
             });
-        }
-
-
-        function now() {
-            var now = new Date();
-            console.log('now: '+now);
         }
 
         /**
@@ -153,7 +168,6 @@ module.exports = function(RED) {
         function statusTime(d) {
             return pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds());
         }
-
         /**
          * helper function to pad the time with zeros
          *
