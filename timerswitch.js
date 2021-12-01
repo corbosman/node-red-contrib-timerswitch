4887a62a464b3000ce8d1be352494a6027a78840
Cor Bosman <cor@xs4all.net>
2021-12-01 22:37:52 +0100

    minor cleanup after PR

--- timerswitch.js
+++ timerswitch.js
@@ -55,7 +55,7 @@ module.exports = function(RED) {
                 node.scheduler.resume();
             } else if (command === 'state') {
         		outputState = null;
-	    }
+	        }
 
             status();
             send(msg);
